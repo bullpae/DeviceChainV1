826c13c2128923893e0b5811d42aa7963ec8bef6
Tom <bullpae@hotmail.com>
2016-09-28 18:27:42 +0900

    delete no use code

--- ng/dashboard.ctrl.js
+++ ng/dashboard.ctrl.js
@@ -1,33 +1,18 @@
 angular.module('app')
 .controller('DashboardCtrl', function ($scope, DashboardSvc) {
-  $scope.dc_list = [
-    {"name":"test1", "host":"192.168.10.20", "port":"9999", "status":"OK"},
-    {"name":"test2", "host":"192.168.10.20", "port":"9999", "status":"OK"},
-    {"name":"test3", "host":"192.168.10.20", "port":"9999", "status":"OK"},
-  ]
-
-  // var dc_node = require("../javascript/dc_node");
-  // $scope.dc_list = dc_node.list;
-
-  // DashboardSvc.fetch()
-  // .then(function (dc_nodes) {
-  //   console.log("test");
-  //   $scope.dc_nodes = dc_nodes;
-  //   console.log("test1");
-  // })
+  // $scope.dc_list = [
+  //   {"name":"test1", "host":"192.168.10.20", "port":"9999", "status":"OK"},
+  //   {"name":"test2", "host":"192.168.10.20", "port":"9999", "status":"OK"},
+  //   {"name":"test3", "host":"192.168.10.20", "port":"9999", "status":"OK"},
+  // ]
 
   $scope.getAuth = function () {
     DashboardSvc.getDcAuth();
   }
 
-  console.log("dashboard.ctrl.js exe");
-  
   DashboardSvc.getNode()
   .then(function (dc_nodes) {
-    console.log("node test1");
-    console.log(dc_nodes);
     $scope.dc_list = dc_nodes;
-    console.log("xxxest1");
   })
 })
 
