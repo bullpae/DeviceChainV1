f3a691ce425ed50db71916736c7baaeb15e01d29
Tom <bullpae@hotmail.com>
2016-10-08 17:20:23 +0900

    add styles.css

--- controllers/static.js
+++ controllers/static.js
@@ -3,6 +3,7 @@ var router  = express.Router()
 
 router.use(express.static(__dirname + '/../assets'))
 router.use('/templates', express.static(__dirname + '/../templates'))
+router.use('/css', express.static(__dirname + '/../css'))
 
 router.get('/', function (req, res) {
   // render/ejs is now easier to use since
