7f4c39ffcf21a5e4cdfff2110ec8d430d80a7f95
Tom <bullpae@hotmail.com>
2016-10-22 00:22:33 +0900

    mod: complete account & address get function

--- controllers/api/account/address_info.js
+++ controllers/api/account/address_info.js
@@ -6,25 +6,26 @@ router.post('/address_info/:userid', function (req, res, next) {
   console.log(req.body)
   console.log("lll address_info post 1: ")
 
-  var Accounts = new AccountInfo()
   var conditions = { userid: req.params.userid }
-  var update = { $set: { 
-      get_address: "true", 
-      public_key: req.body.public_key, 
-      asset_address: req.body.asset_address,
-      watch_only: req.body.watch_only,
-      network: req.body.network, 
-      certstatus: "false",
-      createdate_addr: req.body.created_at }
-  }
-  // var update = req.body
-  AccountInfo.findOneUpdate(conditions, update, 
-  function(err) {
+  // var update = { $set: { 
+  //     get_address: "true", 
+  //     public_key: req.body.account.public_key, 
+  //     asset_address: req.body.account.asset_address,
+  //     watch_only: req.body.account.watch_only,
+  //     network: req.body.account.network, 
+  //     certstatus: "false",
+  //     createdate_addr: req.body.account.created_at }
+  // }
+  var update = { $set: req.body.account }
+  AccountInfo.update(conditions, update) 
+  .exec(function(err) {
     console.log("start update address ")
     
     if (err) res.status(500).json({ error: 'database failure' })
     if (err) { return next(err) }
+    console.log("end update address ")
     res.sendStatus(201)
+    console.log("end ii update address ")
   })
 })
 
@@ -33,43 +34,29 @@ router.delete('/address_info/:userid', function (req, res, next) {
   console.log(req.body)
   console.log("lll address_info post 1: ")
 
-  var Accounts = new AccountInfo()
+  // var Accounts = new AccountInfo()
   var conditions = { userid: req.params.userid }
-  var update = { $set: { 
-      get_address: "false", 
-      public_key: "", 
-      asset_address: "",
-      watch_only: "",
-      network: "", 
-      certstatus: "",
-      createdate_addr: "" }
-  }
-  AccountInfo.findOneUpdate(conditions, update, 
-  function(err) {
-    console.log("start delete address ")
-    
-    if (err) res.status(500).json({ error: 'database failure' })
+  AccountInfo.findOne(conditions)
+  .exec(function (err, account) {
+    console.log(account)
+    console.log("account update test!!!")
     if (err) { return next(err) }
-    res.sendStatus(201)
-  })
-  
-  // Accounts.findById(req.params.userid, function(err, account) {
-  //   account.public_key = ""
-  //   account.asset_address = ""
-  //   account.watch_only = ""
-  //   account.network = ""
-  //   account.certstatus = ""  
-  //   account.createdate_addr = ""    
     
-  //   console.log("delete address info:")
-  //   console.log(account)
-  //   console.log("lll delete address info")
-
-  //   account.save(function (err) {
-  //     if (err) { return next(err) }
-  //     res.sendStatus(201)
-  //   })
-  // })
+    console.log("account update test!!!del")
+    account.get_address = "false"
+    account.public_key =  ""
+    account.asset_address = ""
+    account.watch_only = ""
+    account.network = ""
+    account.certstatus = ""
+    account.createdate_addr = ""
+    
+    account.save(function (err, output) {
+      if (err) { return next(err) }
+      res.send('success');
+    })
+    return res;
+  })
 })
 
 module.exports = router
